8c2efb6b60ea9147ef9becd57c8acfe769349cbd
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2024-02-21 16:09:31 +0100

    cli functionality done

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # joker
-some shit jokes
+
+if you like shit jokes, this is the cli program for you
+
+# Usage
+
+for a random joke:
+
+```sh
+joker
+```
+
+for a joke of a specific category:
+```sh
+joker [category]
+```
+
+to get a list of available categories:
+```sh
+joker -l
+```
--- cmd/root.go
+++ cmd/root.go
@@ -1,34 +1,47 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 El Zubeir Huweidi
 */
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+// Joke represents the structure of a joke
+type Joke struct {
+	Type      string `json:"type"`
+	Setup     string `json:"setup"`
+	Punchline string `json:"punchline"`
+}
 
+var category string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "joker",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "your joke supplier",
+	Long: `joker is a CLI that provides you with a random joke or a joke from a specific category if you specify. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+   joker
+   joker dad
+   joker -l
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if listCategories, _ := cmd.Flags().GetBool("list"); listCategories {
+			getAllCategories()
+		} else if len(args) > 0 {
+			getRandomJoke(args[0])
+		} else {
+			getRandomJoke("")
+		}
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -37,15 +50,76 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.joker.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("list", "l", false, "Get all available joke categories")
 }
 
+func readJokesFromFile() ([]Joke, error) {
+	file, err := os.ReadFile("jokes.json")
+	if err != nil {
+		return nil, err
+	}
 
+	var jokes []Joke
+	err = json.Unmarshal(file, &jokes)
+	if err != nil {
+		return nil, err
+	}
+
+	return jokes, nil
+}
+
+func getRandomJoke(category string) {
+	jokes, err := readJokesFromFile()
+	if err != nil {
+		fmt.Println("Error reading jokes:", err)
+		os.Exit(1)
+	}
+
+	if category != "" {
+		categoryJokes := getCategoryJokes(jokes, category)
+		if len(categoryJokes) == 0 {
+			fmt.Printf("No jokes found for category: %s\n\nUse 'joker -l' to get all available joke categories", category)
+		} else {
+			randomJoke := getRandomJokeFromCategory(categoryJokes)
+			fmt.Printf("%s\n\n%s\n\n", randomJoke.Setup, randomJoke.Punchline)
+		}
+	} else {
+		randomJoke := getRandomJokeFromAllCategories(jokes)
+		fmt.Printf("%s\n\n%s\n\n", randomJoke.Setup, randomJoke.Punchline)
+	}
+}
+
+func getCategoryJokes(jokes []Joke, category string) []Joke {
+	var categoryJokes []Joke
+	for _, joke := range jokes {
+		if joke.Type == category {
+			categoryJokes = append(categoryJokes, joke)
+		}
+	}
+	return categoryJokes
+}
+
+func getAllCategories() {
+	jokes, err := readJokesFromFile()
+	if err != nil {
+		fmt.Println("Error reading jokes:", err)
+		os.Exit(1)
+	}
+
+	uniqueCategories := make(map[string]struct{})
+	for _, joke := range jokes {
+		uniqueCategories[joke.Type] = struct{}{}
+	}
+
+	for category := range uniqueCategories {
+		fmt.Println(category)
+	}
+}
+
+func getRandomJokeFromAllCategories(jokes []Joke) *Joke {
+	return &jokes[rand.Intn(len(jokes))]
+}
+
+func getRandomJokeFromCategory(jokes []Joke) *Joke {
+	return &jokes[rand.Intn(len(jokes))]
+}
